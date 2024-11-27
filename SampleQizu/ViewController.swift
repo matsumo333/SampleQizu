dcbc46c4d13c7305a135fab45c4de4ac1a9ef3cf
matsmo333 <matsumo333@icloud.com>
2024-11-27 20:14:30 +0900

    Initial Commit

---
+++ SampleQizu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleQizu
+//
+//  Created by 松本正 on 2024/11/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SampleQizuTests/SampleQizuTests.swift
@@ -0,0 +1,17 @@
+//
+//  SampleQizuTests.swift
+//  SampleQizuTests
+//
+//  Created by 松本正 on 2024/11/27.
+//
+
+import Testing
+@testable import SampleQizu
+
+struct SampleQizuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
